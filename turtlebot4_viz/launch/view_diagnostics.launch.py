b78f4994b00430033de093bcd7e489d2170b997a
Roni Kreinin <rkreinin@clearpathrobotics.com>
2022-02-15 16:43:14 -0500

    Fixed linter errors

--- turtlebot4_viz/launch/view_diagnostics.launch.py
+++ turtlebot4_viz/launch/view_diagnostics.launch.py
@@ -11,12 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 # @author Roni Kreinin (rkreinin@clearpathrobotics.com)
 
-
 from launch import LaunchDescription
+
 from launch_ros.actions import Node
+
 
 def generate_launch_description():
 

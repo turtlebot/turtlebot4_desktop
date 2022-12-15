805279e4846ec6fd7bdf3f9a08034d361cd6668a
Roni Kreinin <rkreinin@clearpathrobotics.com>
2022-12-15 14:44:40 -0500

    Updated CI to Humble Linter fixes

--- turtlebot4_viz/launch/view_robot.launch.py
+++ turtlebot4_viz/launch/view_robot.launch.py
@@ -17,7 +17,8 @@
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction, GroupAction
+from launch.actions import (DeclareLaunchArgument, GroupAction,
+                            IncludeLaunchDescription, TimerAction)
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
